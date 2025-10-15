8829dc34a07cd41d3424d211f5602e9912215969
rehun92 <60905435+rehun92@users.noreply.github.com>
2025-10-15 21:36:29 +0900

    Update utils_common.R

--- R/utils_common.R
+++ R/utils_common.R
@@ -11,18 +11,18 @@ read_any <- function(path) {
   stop("Unsupported file extension for: ", path)
 }
 
-#' Binarize numeric fold-change/hit scores to 0/1 with threshold and min prevalence
-binarize_hits <- function(dt, id_col, thresh=1L, min_prev=0.01) {
-  stopifnot(id_col %in% names(dt))
-  num_cols <- setdiff(names(dt), id_col)
-  x <- copy(dt)
-  for (j in num_cols) {
-    x[[j]] <- as.integer(x[[j]] >= thresh)
+#' Binarize numeric fold-change/hit scores to 0/1 with threshold
+binarize01_df <- function(df) {
+  out <- as.data.frame(df)
+  for (j in seq_along(out)) {
+    v <- out[[j]]
+    if (is.numeric(v)) {
+      v[is.na(v)] <- 0
+      v[v != 0] <- 1
+      out[[j]] <- v
+    }
   }
-  # drop markers < min_prev
-  prev <- colMeans(x[, ..num_cols], na.rm=TRUE)
-  keep <- names(prev[prev >= min_prev])
-  cbind(x[, ..id_col], x[, ..keep])
+  out
 }
 
 #' Make covariates (harmonize race, sex, smoking, alcohol) as in manuscript
@@ -72,3 +72,66 @@ glm_formula <- function(y, x) {
 
 #' Benjamini–Hochberg FDR
 bh <- function(p) p.adjust(p, method="BH")
+
+#' Short virus names
+species_label_map <- function(x) {
+  x <- gsub('Human alphaherpesvirus 1', 'HSV-1', x)
+  x <- gsub('Human alphaherpesvirus 2', 'HSV-2', x)
+  x <- gsub('Human alphaherpesvirus 3', 'VZV', x)
+  x <- gsub('Human betaherpesvirus 5', 'CMV', x)
+  x <- gsub('Human betaherpesvirus 6A', 'HHV-6A', x)
+  x <- gsub('Human betaherpesvirus 6B', 'HHV-6B', x)
+  x <- gsub('Human betaherpesvirus 7', 'HHV-7', x)
+  x <- gsub('Human gammaherpesvirus 4', 'EBV', x)
+  x <- gsub('Human gammaherpesvirus 8', 'HHV-8', x)
+  x
+}
+
+#' HSV species and color matching
+species_list <- c("HSV-1","HSV-2","VZV","CMV","HHV-6A","HHV-6B","HHV-7","EBV","HHV-8")
+color = c("#F8766D", "#D39200", "#93AA00", "#00BA38", "#00C19F", "#00B9E3", "#619CFF", "#DB72FB", "#FF61C3")
+color_map <- setNames(
+  c("#F8766D","#D39200","#93AA00","#00BA38","#00C19F","#00B9E3","#619CFF","#DB72FB","#FF61C3"),
+  c("HSV-1","HSV-2","VZV","CMV","HHV-6A","HHV-6B","HHV-7","EBV","HHV-8")
+)
+
+
+#' optional cleanups of product names
+clean_replace <- c(
+    "Alkaline nuclease (EC 3.1.-.-)" = "Alkaline nuclease",
+    "Deneddylase (EC 3.4.19.12)" = "Deneddylase",
+    "E3 ubiquitin-protein ligase ICP0 (EC 6.3.2.-)" = "E3 ubiquitin-protein ligase",
+    "Large tegument protein deneddylase (EC 3.4.19.12) (EC 3.4.22.-)" = "Large tegument protein deneddylase",
+    "Nuclear egress protein 2" = "Nuclear egress protein",
+    "Thymidine kinase (EC 2.7.1.21)" = "Thymidine kinase",
+    "US11 *1;US11 *1" = "Protein US11",
+    "Uracil-DNA glycosylase (UDG) (EC 3.2.2.27) (UNG)" = "Uracil-DNA glycosylase",
+    
+    "Envelope glycoprotein UL130" = "Envelope glycoprotein",
+    "Membrane RL1 protein2 (Membrane protein RL12)" = "Membrane protein",
+    "Membrane glycoprotein US3;Membrane glycoprotein US3" = "Membrane glycoprotein",
+    "Membrane glycoprotein US7;Membrane glycoprotein US7" = "Membrane glycoprotein",
+    "Membrane protein RL12;Membrane protein RL12" = "Membrane protein",
+    "Membrane protein RL13;Membrane protein RL13" = "Membrane protein",
+    "Membrane protein RL12" = "Membrane protein",
+    "Membrane protein UL20" = "Membrane protein",
+    "Tegument protein UL43" = "Tegument protein",
+    "UL37" = "Protein UL37",
+    " immediate early glycoprotein" = "",
+    
+    "Envelope glycoprotein gpUL55" = "Envelope glycoprotein",
+    "Immediate early protein " = "",
+    "Membrane RL1 protein2 (Membrane protein RL12)" = "Membrane protein",
+    "Membrane RL1 protein2;Membrane RL1 protein2" = "Membrane protein",
+    "Membrane glycoprotein US7;Membrane glycoprotein US7" = "Membrane glycoprotein",
+    "ORFS326C;ORFS326C;ORFS326C" = "ORFS326C",
+    "Transmembrane protein HWLF3" = "Transmembrane protein",
+    "UL74 protein" = "Protein UL74",
+    
+    "BZLF1 (BZLF1 protein)" = "Transcriptional activator",
+    "EBNA3C (EBNA3C nuclear protein)" = "EBV nuclear antigen",
+    "Nuclear antigen EBNA-3C;Nuclear antigen EBNA-3C" = "EBV nuclear antigen",
+    "Nuclear protein EBNA-2" = "EBV nuclear antigen",
+    "Protein BRRF1;Protein BRRF1;Protein BRRF1" = "Transcriptional activator",
+    "Uncharacterized protein BLRF3" = "Uncharacterized protein"
+)
