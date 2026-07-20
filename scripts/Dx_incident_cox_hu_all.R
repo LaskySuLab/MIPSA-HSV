25e154dd77fd60bce15131a77fd3541fdd006be3
rehun92 <60905435+rehun92@users.noreply.github.com>
2026-07-20 23:05:38 +0900

    Update Dx_incident_cox_hu_all.R

--- scripts/Dx_incident_cox_hu_all.R
+++ scripts/Dx_incident_cox_hu_all.R
@@ -8,12 +8,31 @@ suppressPackageStartupMessages({
 
 # ----------------------------- CLI -----------------------------
 option_list <- list(
-  make_option("--phe",      type="character", help="llf_1289_phe.tsv"),
-  make_option("--vi_bin",   type="character", help="hsv_proteins_binary.txt"),
-  make_option("--hu_bin",   type="character", help="human_fl_binary.txt"),
-  make_option("--dx_count", type="character",
-              help="a directory containing prevalence/incidence files"),
-  make_option("--out_dir",  type="character", default="results/Dx_inc", help="Output dir")
+  # --- LLF Cohort Files ---
+  make_option("--llf_pheno",     type="character", help="Data/llf_1290_phe1.tsv"),
+  make_option("--llf_vi_promax",  type="character", help="Data/IB1007_VirSIGHT_Promax_Hits_Fold-Over-Background.csv"),
+  make_option("--llf_hu_fl",      type="character", help="Data/IB1007_HuSIGHT_FullLength_Hits_Fold-Over-Background.csv"),
+  make_option("--llf_vi_bin",     type="character", help="Data/hsv_promax_bin_MGBB-LLF.tsv"),
+  make_option("--llf_hu_bin",     type="character", help="Data/human_fl_bin_MGBB-LLF.tsv"),
+  make_option("--rep_llf",        type="character", help="results1/Run/hsv_bin_fchange_rep_llf.tsv"),
+  make_option("--llf_dx_count",   type="character", help="results1/Figure1/llf_case_counts_filtered.csv"),
+
+  # --- LEC Cohort Files ---
+  make_option("--lec_pheno",     type="character", help="Data/lec_763_phe1.tsv"),
+  make_option("--lec_vi_bin",     type="character", help="Data/hsv_promax_bin_MGBB-LEC.tsv"),
+  make_option("--lec_hu_bin",     type="character", help="Data/human_fl_bin_MGBB-LEC.tsv"),
+  make_option("--rep_lec",        type="character", help="results1/Run/hsv_bin_fchange_rep_lec.tsv"),
+  make_option("--lec_dx_count",   type="character", help="results1/Figure1/lec_case_counts_filtered.csv"),
+
+  # --- Shared / Other Data ---
+  make_option("--abc_vi_promax",  type="character", help="Data/IB1021_VirSIGHT_Promax_Hits_Fold-Over-Background.csv"),
+  make_option("--abc_hu_fl",      type="character", help="Data/IB1021_HuSIGHT_FullLength_Hits_Fold-Over-Background.csv"),
+  make_option("--leo_vi_promax",  type="character", help="Data/IB1189_VirSIGHT_Promax_Hits_Fold-Over-Background_Old-Format-HSV.tsv"),
+  make_option("--leo_hu_fl",      type="character", help="Data/IB1189_HuSIGHT_FullLength_Hits_Fold-Over-Background.tsv"),
+  make_option("--sig_ab",         type="character", help="results1/Figure4/lec_test_auc_summary_sig.csv"),
+
+  # --- Output Directory ---
+  make_option("--out_dir",        type="character", default="results1/Dx_inc", help="Output dir")
 )
 opt <- parse_args(OptionParser(option_list=option_list))
 dir.create(opt$out_dir, showWarnings = FALSE, recursive = TRUE)
@@ -46,8 +65,8 @@ run_cox_human <- function(ii, cohort_name, input_dt) {
   control_pos_vec <- colSums(input_dt[control_idx, ..hu_var] > 1, na.rm = TRUE)
   
   if (cohort_name=='llf'){
-    covars <- "Age_at_collect + Gender + BMI_most_recent + Race_Group + Smoking + Alcohol + ics_trim_totnum_5y + cci"
-  } else {covars <- "Age_at_collect + Gender + BMI_most_recent + Race_Group + Smoking + Alcohol + cci + cohort"}
+    covars <- "Age_at_collect + Gender + BMI_most_recent + Race_Group + Smoking + Alcohol + ics_trim_totnum_5y + cci + rx_immunosuppressant_24m + rx_antiviral_24m + rx_antineoplastic_24m"
+  } else {covars <- "Age_at_collect + Gender + BMI_most_recent + Race_Group + Smoking + Alcohol + cci + cohort + rx_immunosuppressant_24m + rx_antiviral_24m + rx_antineoplastic_24m"}
   
   ## ---------- 2. Per-Protein Loop -----------------------------------------
   cox_results <- lapply(seq_along(hu_var), function(j) {
@@ -196,7 +215,6 @@ llf_phe1 <- build_disease_combine(llf_phe)
 sig_ab <- fread(opt$sig_ab)
 
 # MIPSA files
-llf_vi_promax <- fread(opt$llf_vi_promax)
 llf_hu_fl <- fread(opt$llf_hu_fl)
 if (!"var_id" %in% names(llf_hu_fl)) llf_hu_fl[, var_id := paste0("h", seq_len(.N))]
 
@@ -216,7 +234,7 @@ merge_dt_raw.llf <- llf_phe1 %>%
 # ----------------------------- Run COX -----------------------------
 # Human FL predictors (all antibodies)
 rep_llf = fread(opt$rep_llf)
-hu_var <- unique(rep_llf$antibody) #117
+hu_var <- unique(rep_llf$antibody) #120
 
 # ----------------------------- Disease cols -----------------------------
 dx_cols <- grep("_combine$", colnames(merge_dt_raw.llf), value = TRUE)
@@ -227,13 +245,13 @@ dx_sums <- colSums(merge_dt_raw.llf[, ..dx_cols], na.rm = TRUE)
 dx_list <- names(dx_sums[dx_sums >= 12.89])
 ex_list <- c(default="Depression","Opioid_use_disorder","Alcoholism","toothache","sciatica","Psychosis","loose_tooth",
              "Asthma","Headache","Migraine","Post_Traumatic_Stress_Disorder","Bipolar_Disorder","Female_Infertility","Male_Infertility",
-             "Chronic viral hepatitis","Schizophrenia","Chronic_viral_hepatitis","Other_chronic_heptitis","Alcohol_liver_disease","Hiatus_Hernia",
-             "Diverticular","Cataract","Presbyopia","Mouth_ulcer")
-dx_list <- setdiff(dx_list, paste0(ex_list,"_combine")) # 73 diseases
+             "Schizophrenia","Chronic_viral_hepatitis","Other_chronic_heptitis","Alcohol_liver_disease","Hiatus_Hernia",
+             "Diverticular","Cataract","Presbyopia","Mouth_ulcer","Lyme_Disease")
+dx_list <- setdiff(dx_list, paste0(ex_list,"_combine")) # 76 diseases
 dx_list <- gsub("_combine", "", dx_list)
 dx_list <- paste0(dx_list,"_flag")
 
-lapply(1:length(dx_list), run_cox_human, cohort_name = "llf", input_dt = merge_dt_raw.llf)
+# lapply(1:length(dx_list), run_cox_human, cohort_name = "llf", input_dt = merge_dt_raw.llf)
 
 # ----------------------------- IO in LEC------------------------------
 # Phenotype table and covariate processing in LEC
@@ -241,11 +259,9 @@ lec_phe <- fread(opt$lec_pheno)
 lec_phe1 <- build_disease_combine(lec_phe)
 
 # MIPSA files
-abc_vi_promax <- fread(opt$abc_vi_promax)
 abc_hu_fl <- fread(opt$abc_hu_fl)
 if (!"var_id" %in% names(abc_hu_fl)) abc_hu_fl[, var_id := paste0("h", .I)]
 
-leo_vi_promax <- fread(opt$leo_vi_promax)
 leo_hu_fl <- fread(opt$leo_hu_fl)
 
 abc_hu_fl1 = subset(abc_hu_fl, abc_hu_fl$pep_id%in%leo_hu_fl$pep_id)
@@ -271,12 +287,33 @@ merge_dt_raw.lec <- lec_phe1 %>%
 dx_test <- colSums(merge_dt_raw.lec[, ..dx_list], na.rm = TRUE)
 length(dx_test)==length(dx_list)
 
-lapply(1:length(dx_list), run_cox_human, cohort_name = "lec", input_dt = merge_dt_raw.lec)
+# lapply(1:length(dx_list), run_cox_human, cohort_name = "lec", input_dt = merge_dt_raw.lec)
 
-# ----------------------------- IO in Pooled LLF-LEC-----------------------------
+# ----------------------------- IO in Pooled LLF-LEC------------------------------
+
 common_names = intersect(colnames(merge_dt_raw.lec), colnames(merge_dt_raw.llf))
 
-merge_dt_raw.all = rbind(merge_dt_raw.llf[, ..common_names], merge_dt_raw.lec[, ..common_names]) #2052
+# 1. Split common_names into chunks of 5000
+chunk_size <- 5000
+col_chunks <- split(common_names, ceiling(seq_along(common_names) / chunk_size))
+
+# 2. Merge each chunk separately
+merged_chunks <- lapply(col_chunks, function(cols) {
+  # rbind only a small subset of columns at a time
+  dt_chunk <- rbind(
+    merge_dt_raw.llf[, ..cols], 
+    merge_dt_raw.lec[, ..cols]
+  )
+  return(dt_chunk)
+})
+
+# 3. Combine the chunks side-by-side
+merge_dt_raw.all <- do.call(cbind, merged_chunks) #2052
+setnames(merge_dt_raw.all, gsub("^[0-9]+\\.", "", names(merge_dt_raw.all)))
+
+# 4. Clean up to free memory
+rm(merged_chunks)
+gc()
 
 dx_test <- colSums(merge_dt_raw.all[, ..dx_list], na.rm = TRUE)
 length(dx_test)==length(dx_list)
@@ -290,6 +327,7 @@ hu.dx_all <- hu.dx_all[,-c('file')]
 hu.dx_all$disease <- gsub('_flag', '', hu.dx_all$disease) 
 hu.dx_all = inner_join(llf_hu_fl[,c('var_id', 'gene_symbol','UniProt_acc', 'product')], hu.dx_all, by=c('var_id'))
 hu.dx_all$gene_symbol <- gsub(" \\s*\\([^\\)]+\\)", "",  hu.dx_all$gene_symbol)
+hu.dx_all$gene_symbol <- gsub("GPRASP3,ARMCX5-GPRASP2", "GPRASP3",  hu.dx_all$gene_symbol)
 hu.dx_all$gene_symbol <- gsub("ZNF559-ZNF177,ZNF177", "ZNF177",  hu.dx_all$gene_symbol)
 
 ## Reshape data to wide format 
@@ -303,252 +341,11 @@ res_wide = left_join(res_wide, dx_lookup, by=c('disease'='Disease'))
 res_wide$disease = gsub('_', ' ', res_wide$disease)
 res_wide$Category = gsub('_', ' ', res_wide$Category)
 
-res_wide$p.adj_lec = p.adjust(res_wide$P_lec, method='fdr')
 res_wide$p.adj_all = p.adjust(res_wide$P_all, method='fdr')
 res_wide$p.adj_llf = p.adjust(res_wide$P_llf, method='fdr')
+res_wide$p.adj_lec = p.adjust(res_wide$P_lec, method='fdr')
 
 fwrite(res_wide,
        file.path(opt$out_dir, "rep_hu_dx_all_inc_coxphf_ann.tsv"),
        sep = "\t", quote = FALSE)
 
-res_wide = fread(file.path(opt$out_dir, "rep_hu_dx_all_inc_coxphf_ann.tsv"), head=T)
-
-# Classify Significance
-res_wide[, Sig_Status := fcase(
-  P_lec < 0.05 & P_llf < 0.05, "Both Significant",
-  P_lec < 0.05,                "MGBB-LEC Only",
-  P_llf < 0.05,                "MGBB-LLF Only",
-  default =                    "Not Significant"
-)]
-
-# Plot
-cutoff_p <- {
-  sig_idx <- which(res_wide$p.adj_all < 0.05)
-  if (length(sig_idx)) max(res_wide$P_all[sig_idx], na.rm = TRUE) else NULL
-} #0.0004020097
-
-p.all <- ggplot(res_wide, aes(x = disease, y = -log10(P_all))) +
-  facet_grid(. ~ Category, scales = "free_x") +
-  geom_point(aes(color = HR_all), size = 2) +
-  scale_color_gradient2(low = "#2b83ba", mid = "#FFFFB3", high = "#d7191c",
-                        midpoint = 1, limits = c(0, 2), oob = scales::squish) +
-  geom_hline(yintercept = -log10(cutoff_p), color = "black", linetype = "dashed") +
-  geom_hline(yintercept = -log10(0.05), color = "black", linetype = "solid") +
-  labs(title = 'The Replicated Human Autoantigens for Incident Diseases',
-       y = expression('-log'[10]*'('*italic(P)*'-value)'),
-       color = "HR") +
-  theme_bw() +
-  theme(
-    panel.border  = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.position = "bottom",
-    legend.text  = element_text(size = 8),
-    plot.title   = element_text(hjust = 0.5, size = 11),
-    strip.text.x = element_text(size = 10),
-    axis.title.x = element_text(size = 10),
-    axis.text.x  = element_text(angle = 70, hjust = 1, size = 8),
-    axis.text.y  = element_text(size = 10),
-    axis.title.y = element_text(size = 10)
-  )
-
-p.all <- p.all + ggrepel::geom_label_repel(
-  data = subset(res_wide, P_all < cutoff_p),
-  aes(label = gene_symbol),
-  size = 2, segment.size = 0.1, direction = "both",
-  segment.color = 'black', max.overlaps = 20
-)
-
-ggsave(file.path(opt$out_dir, "figure5_hr_cox_all.png"), p.all, width = 12, height = 7, dpi = 300, bg='white')
-
-# HR significance comparison
-p.hr <- ggplot(res_wide, aes(x = disease, y = -log10(P_all))) +
-  facet_grid(. ~ Category, scales = "free_x") +
-  geom_point(aes(color = Sig_Status), size = 2) +
-  scale_color_manual(values = c("Both Significant" = "red", 
-                                "MGBB-LEC Only" = "blue", 
-                                "MGBB-LLF Only" = "green", 
-                                "Not Significant" = "grey80")) +
-  geom_hline(yintercept = -log10(cutoff_p), color = "black", linetype = "dashed") +
-  geom_hline(yintercept = -log10(0.05), color = "black", linetype = "solid") +
-  labs(title = 'The Replicated Human Autoantigens for Incident Diseases',
-       y = expression('-log'[10]*'('*italic(P)*'-value)'),
-       color = "HR") +
-  theme_bw() +
-  theme(
-    panel.border  = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.position = "bottom",
-    legend.text  = element_text(size = 8),
-    plot.title   = element_text(hjust = 0.5, size = 11),
-    strip.text.x = element_text(size = 10),
-    axis.title.x = element_text(size = 10),
-    axis.text.x  = element_text(angle = 70, hjust = 1, size = 8),
-    axis.text.y  = element_text(size = 10),
-    axis.title.y = element_text(size = 10)
-  )
-
-p.hr <- p.hr + ggrepel::geom_label_repel(
-  data = subset(res_wide, P_all < cutoff_p),
-  aes(label = gene_symbol),
-  size = 2, segment.size = 0.1, direction = "both",
-  segment.color = 'black', max.overlaps = 20
-)
-ggsave(file.path(opt$out_dir, "figure5_hr_comparison_cox_all.png"), p.hr, width = 12, height = 7, dpi = 300, bg='white')
-
-# Beta comparison Plot
-p.beta = ggplot(res_wide, aes(x = Beta_lec, y = Beta_llf, color = Sig_Status)) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "gray") +
-  geom_vline(xintercept = 0, linetype = "dashed", color = "gray") +
-  geom_abline(intercept = 0, slope = 1, linetype = "dotted") + # Perfect replication line
-  geom_point(alpha = 0.7, size = 2) +
-  scale_color_manual(values = c("Both Significant" = "red", 
-                                "MGBB-LEC Only" = "blue", 
-                                "MGBB-LLF Only" = "green", 
-                                "Not Significant" = "grey80")) +
-  theme_minimal() +
-  labs(title = "Cohort comparison between MGBB-LLF and MGBB-LEC in Incident diseases",
-       subtitle = "Checking for consistency of effect direction",
-       x = "Effect Size (MGBB-LEC)", 
-       y = "Effect Size (MGBB-LLF)") +
-  theme(
-    panel.border  = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.position = "bottom",
-    legend.text  = element_text(size = 10),
-    plot.title   = element_text(size = 12),
-    axis.title.x = element_text(size = 10),
-    axis.text.x  = element_text(size = 8),
-    axis.text.y  = element_text(size = 10),
-    axis.title.y = element_text(size = 10)
-  ) +
-  # Optional: Label only the top significant hits to avoid clutter
-  ggrepel::geom_label_repel(data = res_wide[Sig_Status == "Both Significant"], 
-                  aes(label = paste(disease, gene_symbol, sep="-")),
-                  size = 3, segment.size = 0.1, direction = "both",
-                  segment.color = 'black', max.overlaps = 20)
-
-ggsave(file.path(opt$out_dir, "figure5_beta_cox_all.png"), p.beta, width = 12, height = 10, dpi = 300, bg='white')
-
-# ----------------------------- Collect Data: Human in LLF & LEC -----------------------------
-
-hu.dx_com <- read_results("^bin_fchange_hu_.*_(llf|lec)\\.csv$")
-hu.dx_com <- hu.dx_com[,-c('file')]
-hu.dx_com$disease <- gsub('_flag', '', hu.dx_com$disease) 
-hu.dx_com = inner_join(llf_hu_anno[,c('var_id', 'gene_symbol', 'product')], hu.dx_com, by=c('var_id'))
-hu.dx_com$gene_symbol <- gsub(" \\s*\\([^\\)]+\\)", "",  hu.dx_com$gene_symbol)
-
-fwrite(hu.dx_com,
-       file.path(opt$out_dir, "rep_hu_dx_llf_lec_inc_coxphf_ann.tsv"),
-       sep = "\t", quote = FALSE)
-
-hu.dx_com = fread(file.path(opt$out_dir, "rep_hu_dx_llf_lec_inc_coxphf_ann.tsv"), head=T)
-
-# Meta-analysis
-# 1. Reshape data to wide format 
-res_wide <- dcast(hu.dx_com, disease + var_id + gene_symbol +product ~ cohort, 
-                  value.var = c("Beta","SE","Z", "P","n_case",'n_control',"case_pos", "control_pos"))
-# Meta-analysis
-# Initialize an empty list to store results
-pooled_results <- list()
-
-for(i in 1:nrow(res_wide)) {
-  
-  # 1. Create temporary data for this specific gene
-  gene_id <- res_wide$gene_symbol[i]
-  dx_id <- res_wide$disease[i]
-  print(paste("Processing:", dx_id,"| Gene:", gene_id))
-  
-  meta_df <- data.frame(
-    study = c("llf", "lec"),
-    TE    = c(res_wide$Beta_llf[i], res_wide$Beta_lec[i]),
-    seTE  = c(res_wide$SE_llf[i],   res_wide$SE_lec[i])
-  )
-  
-  # 2. Run Meta-Analysis
-  m <- metagen(TE = TE, seTE = seTE, data = meta_df, 
-               studlab = study, sm = "HR",
-               fixed = TRUE, random = TRUE, method.tau = "REML")
-  
-  # 3. Extract the Pooled Results (Fixed Effect example)
-  # If you prefer Random Effects, use m$TE.random, m$lower.random, etc.
-  pooled_results[[i]] <- data.frame(
-    Gene      = gene_id,
-    Disease   = dx_id,
-    Pooled_HR    = exp(m$TE.fixed),        # Exponentiate to get HR
-    Lower_CI     = exp(m$lower.fixed),
-    Upper_CI     = exp(m$upper.fixed),
-    P_value      = m$pval.fixed,
-    I2_Heterogeneity = m$I2                # Check consistency between cohorts
-  )
-}
-
-
-# SCombine and View Final Table
-
-final_table <- do.call(rbind, pooled_results)
-final_table$p.adj = p.adjust(final_table$P_value, method='fdr')
-final_table.sig = subset(final_table, final_table$p.adj<0.05) #162
-
-dx_lookup <- stack(dx_categories)
-names(dx_lookup) <- c("Disease", "Category")
-final_table = left_join(final_table, dx_lookup, by=c('Disease'))
-
-final_table$Disease = gsub('_', ' ', final_table$Disease)
-final_table$Category = gsub('_', ' ', final_table$Category)
-
-fwrite(final_table,
-       file.path(opt$out_dir, "meta_hu_dx_inc_cox_ann_100.tsv"),
-       sep = "\t", quote = FALSE)
-
-final_table_hid = subset(final_table, final_table$Gene%in%sig_ab$Gene) # AUC > 85%
-final_table_hid$p.adj = p.adjust(final_table_hid$P_value, method='fdr')
-final_table_hid.sig = subset(final_table_hid, final_table_hid$p.adj<0.05) #27 (85%)
-
-fwrite(final_table_hid,
-       file.path(opt$out_dir, "meta_hu_dx_inc_cox_ann.tsv"),
-       sep = "\t", quote = FALSE)
-
-final_table = fread(file.path(opt$out_dir, "meta_hu_dx_inc_cox_ann_100.tsv"), head=T)
-
-# Plot
-cutoff_p <- {
-  sig_idx <- which(final_table$p.adj <= 0.05)
-  if (length(sig_idx)) max(final_table$P_value[sig_idx], na.rm = TRUE) else NULL
-}
-
-p <- ggplot(final_table, aes(x = Disease, y = -log10(P_value))) +
-  facet_grid(. ~ Category, scales = "free_x") +
-  geom_point(aes(color = Pooled_HR), size = 2) +
-  scale_color_gradient2(low = "#2b83ba", mid = "#FFFFB3", high = "#d7191c",
-                        midpoint = 1, limits = c(0, 2), oob = scales::squish) +
-  geom_hline(yintercept = -log10(cutoff_p), color = "black", linetype = "dashed") +
-  geom_hline(yintercept = -log10(0.05), color = "black", linetype = "solid") +
-  labs(title = 'The Replicated Human Autoantigens for Incident Diseases',
-       y = expression('-log'[10]*'('*italic(P)*'-value)'),
-       color = "HR") +
-  theme_bw() +
-  theme(
-    panel.border  = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.position = "bottom",
-    legend.text  = element_text(size = 8),
-    plot.title   = element_text(hjust = 0.5, size = 11),
-    strip.text.x = element_text(size = 10),
-    axis.title.x = element_text(size = 10),
-    axis.text.x  = element_text(angle = 70, hjust = 1, size = 8),
-    axis.text.y  = element_text(size = 10),
-    axis.title.y = element_text(size = 10)
-  )
-
-p <- p + ggrepel::geom_label_repel(
-  data = subset(final_table, P_value < 0.001),
-  aes(label = Gene),
-  size = 2, segment.size = 0.1, direction = "both",
-  segment.color = 'black', max.overlaps = 20
-)
-
-ggsave(file.path(opt$out_dir, "figure5_meta_cox_100.png"), p, width = 12, height = 7, dpi = 300, bg='white')
